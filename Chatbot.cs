fb4fd5bb96c1e0ad8d42bdcbfc1845cf2cbff4cc
Mpumelelo <St10449316@vcconnect.edu.za>
2025-03-22 14:43:55 +0200

    Add project files.

---
+++ Chatbot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurity_Awareness_chatbot
+{
+    internal class Chatbot
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Threading;
+using CyberSecurity_Awareness_chatbot;
+
+namespace CyberSecurity_Awareness_Chatbot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "CyberSecurity Awareness Chatbot";  // Set Console Title
+            ASCIIArt.ShowLogo();       // Show ASCII Logo
+            VoiceGreeting.PlayGreeting();  // Play Voice Greeting
+            Chatbot.StartChat();       // Start Chatbot Interaction
+        }
+    }
+}
